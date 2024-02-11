5ea820feac8c5b777adf110a5dc473d6a469245f
tajhmartin1 <tajhmartin87@gmail.com>
2024-02-10 19:50:50 -0500

    finished about page

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,24 +1,25 @@
-import React from 'react'
+import React, { } from 'react';
 import './about.css';
-function About(){
-    return(
-        <div>
-            <h1>
-                About
-            </h1>
-            <hr className='width'></hr>
-            <p>
-            WBAR is the college radio station of Barnard College in New York City WBAR was developed in the early 1990s as a freeform alter- native to WKCR-FM on the Columbia University campus. Since then, WBAR has provided community and a form of expression for music and podcast-loving Barnard College and Columbia University students seeking a place to play the music that impassions them.
+const image = require('./about.png');
 
-Every semester, our board members hand pick the semester schedule with a mix of recurring and brand new DJs to continuously diversify our schedule. From Southern hip hop to Russian techno, there is no limit to how the nearly 100 DJs fill their weekly 2 hour time slots.
-            </p>
-            <br>
-            </br>
-            <p>
-            We broadcast 22 hours a day (with a daily 4-6am “open time slot”) to listeners all over the world.
-In addition to our online broadcast, WBAR hosts several live concerts per year in Morningside Heights, including the WBAR-B-Q, our annual spring outdoor concert, as well as a Winter Formal concert event in December. Recent perform- ers have included Junglepussy, Frost Children, Kari Faux, Dorian Electra, Spirit of the Beehive, Coco & Clair Clair, Palehound, Liturgy, Japanese Breakfast, Ravyn Lenae, and more.
-            </p>
-        </div>
-    )
+function About() {
+
+
+  return (
+    <div className="About" style={{
+      position: 'relative',
+    }}>
+      <img
+        src={image}
+        alt="Background"
+        style={{
+          width: '90%',
+      
+        }}
+      />
+      {/* Your content here */}
+    </div>
+  );
 }
-export default About
+
+export default About;
