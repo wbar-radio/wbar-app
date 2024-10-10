f856dd4bdd221234dad096bac90ef4fd2309c8c5
Ava Hajratwala <100488972+avahajr@users.noreply.github.com>
2024-10-10 15:03:22 -0400

    remove bugged imports;

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,6 @@ import StickyStream from './Components/StickyStream';
 import Chat from './Components/Chat';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css.map';
-import SpinitronWidget from './Components/SpinitronWidget';
-import SpinitronData from './Components/SpinitronData';
 import {
   BrowserRouter as Router,
   Routes,
