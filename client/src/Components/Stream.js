c04a13cc6467bbd435c5f097e096be09dda6cd13
Ava Hajratwala <100488972+avahajr@users.noreply.github.com>
2024-11-02 20:31:05 -0400

    dynamic waveform scaling;

--- client/src/Components/Stream.js
+++ client/src/Components/Stream.js
@@ -1,15 +1,20 @@
 import React, {useRef, useState, useEffect} from 'react';
 import './Stream.css'
+
 function Stream() {
     const audioRef = useRef(null);
     const canvasRef = useRef(null);
     const [isPlaying, setIsPlaying] = useState(false);
     const [volume, setVolume] = useState(.5);
+    const [averageAmplitude, setAverageAmplitude] = useState(0);
     const ctx = useRef(null);
     const volumeGainRef = useRef(null);
     const waveformGainRef = useRef(null);
     const sourceNode = useRef(null);
     const analyser = useRef(null);
+
+    const minDesiredAmplitude = 0.15;
+    const maxDesiredAmplitude = 0.4;
 
     useEffect(() => {
         if (audioRef.current) {
@@ -17,7 +22,7 @@ function Stream() {
                 ctx.current = new (window.AudioContext || window.webkitAudioContext)();
                 volumeGainRef.current = ctx.current.createGain();
                 waveformGainRef.current = ctx.current.createGain();
-                waveformGainRef.current.gain.value =3;
+                waveformGainRef.current.gain.value = 3;
             }
 
             audioRef.current.crossOrigin = "anonymous";
@@ -30,7 +35,7 @@ function Stream() {
             if (!analyser.current) {
                 analyser.current = ctx.current.createAnalyser();
                 analyser.current.fftSize = 256;
-                analyser.current.smoothingTimeConstant = 0.5;
+                analyser.current.smoothingTimeConstant = 0.9;
             }
 
             if (sourceNode.current && waveformGainRef.current && analyser.current) {
@@ -68,49 +73,99 @@ function Stream() {
         setVolume(event.target.value);
     };
 
-    function drawWaveform(analyser) {
-        const canvas = canvasRef.current;
-        const canvasCtx = canvas.getContext('2d');
-        const bufferLength = analyser.frequencyBinCount;
-        const dataArray = new Uint8Array(bufferLength);
 
-        function draw() {
-            requestAnimationFrame(draw);
 
-            canvasCtx.fillStyle = 'rgb(0, 0, 0)';
-            canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
+function drawWaveform(analyser) {
+    const canvas = canvasRef.current;
+    const canvasCtx = canvas.getContext('2d');
+    const bufferLength = analyser.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
 
-            const barWidth = (canvas.width / bufferLength) * 2.5;
+    let belowDesiredStartTime = null;
+    let aboveDesiredStartTime = null;
 
-            if (!isPlaying) {
-                // Draw a horizontal line when paused
-                canvasCtx.strokeStyle = 'rgba(155,40,221,0.71)';
-                canvasCtx.lineWidth = barWidth;
-                canvasCtx.beginPath();
-                canvasCtx.moveTo(0, canvas.height / 2);
-                canvasCtx.lineTo(canvas.width, canvas.height / 2);
-                canvasCtx.stroke();
-                return;
-            }
+    function draw() {
+        requestAnimationFrame(draw);
 
-            analyser.getByteTimeDomainData(dataArray);
-            let x = 0;
+        canvasCtx.fillStyle = 'rgb(0, 0, 0)';
+        canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
 
-            for (let i = 0; i < bufferLength; i++) {
-                const v = dataArray[i] / 128.0;
-                const y = v * canvas.height / 2;
-                const barHeight = Math.abs(y - canvas.height / 2) * 2;
+        const barWidth = (canvas.width / bufferLength) * 2.5;
 
-                canvasCtx.fillStyle = 'rgb(155,40,221)';
-                canvasCtx.fillRect(x, canvas.height / 2 - barHeight / 2, barWidth, barHeight);
-
-                x += barWidth + 1;
-            }
+        if (!isPlaying) {
+            // Draw a horizontal line when paused
+            canvasCtx.strokeStyle = 'rgba(155,40,221,0.71)';
+            canvasCtx.lineWidth = barWidth;
+            canvasCtx.beginPath();
+            canvasCtx.moveTo(0, canvas.height / 2);
+            canvasCtx.lineTo(canvas.width, canvas.height / 2);
+            canvasCtx.stroke();
+            return;
         }
 
-        draw();
+        analyser.getByteTimeDomainData(dataArray);
+        let x = 0;
+        let sum = 0;
+
+        for (let i = 0; i < bufferLength; i++) {
+            const v = dataArray[i] / 128.0;
+            const y = v * canvas.height / 2;
+            const barHeight = Math.abs(y - canvas.height / 2) * 2;
+
+            canvasCtx.fillStyle = 'rgb(155,40,221)';
+            canvasCtx.fillRect(x, canvas.height / 2 - barHeight / 2, barWidth, barHeight);
+
+            x += barWidth + 1;
+            sum += Math.abs(v - 1); // Calculate the amplitude
+        }
+
+        const avgAmplitude = sum / bufferLength;
+        setAverageAmplitude(avgAmplitude);
+
+        // Track the time when the average amplitude is below the desired amplitude
+        if (avgAmplitude < minDesiredAmplitude) {
+            if (!belowDesiredStartTime) {
+                belowDesiredStartTime = Date.now();
+            } else if (Date.now() - belowDesiredStartTime >= 15000) { // 15 sec
+                console.log("adjusting gain")
+                if (waveformGainRef.current) {
+                    const gainValue = minDesiredAmplitude / avgAmplitude;
+                    if (isFinite(gainValue)) {
+                        const currentTime = ctx.current.currentTime;
+                        waveformGainRef.current.gain.setValueAtTime(parseFloat(gainValue.toFixed(2)), currentTime + 0.1);
+                    } else {
+                        console.error("Invalid gain value:", gainValue);
+                    }
+                }
+                belowDesiredStartTime = null; // Reset the timer
+            }
+        } else {
+            belowDesiredStartTime = null; // Reset the timer if the average amplitude is above the desired amplitude
+        }
+
+        if (avgAmplitude > maxDesiredAmplitude) {
+            if (!aboveDesiredStartTime) {
+                aboveDesiredStartTime = Date.now();
+            } else if (Date.now() - aboveDesiredStartTime >= 15000) { // 15 sec
+                console.log("adjusting gain down");
+                if (waveformGainRef.current) {
+                    const gainValue = maxDesiredAmplitude / avgAmplitude;
+                    if (isFinite(gainValue)) {
+                        const currentTime = ctx.current.currentTime;
+                        waveformGainRef.current.gain.setValueAtTime(parseFloat(gainValue.toFixed(2)), currentTime + 0.1);
+                    } else {
+                        console.error("Invalid gain value:", gainValue);
+                    }
+                }
+                aboveDesiredStartTime = null; // Reset the timer
+            }
+        } else {
+            aboveDesiredStartTime = null; // Reset the timer if the average amplitude is below the max desired amplitude
+        }
     }
 
+    draw();
+}
     return (
         <div className={'d-flex justify-content-center'}>
             <div id="stream-box">
@@ -121,15 +176,10 @@ function Stream() {
                     <i className={`bi h3 ${isPlaying ? "bi-stop-circle-fill" : "bi-play-circle-fill"} text-white`}
                        onClick={handleTogglePlayState}/>
                     <canvas id="waveform-canvas" ref={canvasRef} width="100em" height="40em"></canvas>
-
                 </div>
-                {/*<div>*/}
-                {/*    <input type="range" min="0" max="2" step="0.01" value={volume} onChange={handleVolumeChange}/>*/}
-                {/*</div>*/}
             </div>
-
-        </div>)
-        ;
+        </div>
+    );
 }
 
 export default Stream;
