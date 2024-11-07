23b6a64639e3eca031dcf65992acebba437816f9
Ava Hajratwala <100488972+avahajr@users.noreply.github.com>
2024-11-06 20:19:11 -0500

    cleanup Waveform.js

--- client/src/Components/Waveform.js
+++ client/src/Components/Waveform.js
@@ -1,6 +1,6 @@
 import React, { useRef, useEffect } from 'react';
 
-function Waveform({ isPlaying, analyser, gainNode, minDesiredAmplitude, maxDesiredAmplitude, setAverageAmplitude }) {
+function Waveform({ isPlaying, analyser }) {
     const canvasRef = useRef(null);
 
     useEffect(() => {
@@ -30,8 +30,6 @@ function Waveform({ isPlaying, analyser, gainNode, minDesiredAmplitude, maxDesir
         const bufferLength = analyser.frequencyBinCount;
         const dataArray = new Uint8Array(bufferLength);
 
-        let belowDesiredStartTime = null;
-        let aboveDesiredStartTime = null;
 
         function draw() {
             requestAnimationFrame(draw);
@@ -67,50 +65,6 @@ function Waveform({ isPlaying, analyser, gainNode, minDesiredAmplitude, maxDesir
                 x += barWidth + 1;
                 sum += Math.abs(v - 1); // Calculate the amplitude
             }
-
-        //     const avgAmplitude = sum / bufferLength;
-        //     setAverageAmplitude(avgAmplitude);
-        //
-        //     // Track the time when the average amplitude is below the desired amplitude
-        //     if (avgAmplitude < minDesiredAmplitude) {
-        //         if (!belowDesiredStartTime) {
-        //             belowDesiredStartTime = Date.now();
-        //         } else if (Date.now() - belowDesiredStartTime >= 15000) { // 15 sec
-        //             console.log("adjusting gain")
-        //             if (gainNode) {
-        //                 const gainValue = minDesiredAmplitude / avgAmplitude;
-        //                 if (isFinite(gainValue)) {
-        //                     const currentTime = gainNode.context.currentTime;
-        //                     gainNode.gain.setValueAtTime(parseFloat(gainValue.toFixed(2)), currentTime + 0.1);
-        //                 } else {
-        //                     console.error("Invalid gain value:", gainValue);
-        //                 }
-        //             }
-        //             belowDesiredStartTime = null; // Reset the timer
-        //         }
-        //     } else {
-        //         belowDesiredStartTime = null; // Reset the timer if the average amplitude is above the desired amplitude
-        //     }
-        //
-        //     if (avgAmplitude > maxDesiredAmplitude) {
-        //         if (!aboveDesiredStartTime) {
-        //             aboveDesiredStartTime = Date.now();
-        //         } else if (Date.now() - aboveDesiredStartTime >= 15000) { // 15 sec
-        //             console.log("adjusting gain down");
-        //             if (gainNode) {
-        //                 const gainValue = maxDesiredAmplitude / avgAmplitude;
-        //                 if (isFinite(gainValue)) {
-        //                     const currentTime = gainNode.context.currentTime;
-        //                     gainNode.gain.setValueAtTime(parseFloat(gainValue.toFixed(2)), currentTime + 0.1);
-        //                 } else {
-        //                     console.error("Invalid gain value:", gainValue);
-        //                 }
-        //             }
-        //             aboveDesiredStartTime = null; // Reset the timer
-        //         }
-        //     } else {
-        //         aboveDesiredStartTime = null; // Reset the timer if the average amplitude is below the max desired amplitude
-        //     }
         }
 
         draw();
