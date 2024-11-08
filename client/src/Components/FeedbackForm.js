1a7f40e31861fbf1b152a6163bc7c2942a5670f9
Ava Hajratwala <100488972+avahajr@users.noreply.github.com>
2024-11-07 22:44:30 -0500

    add form desc and hyperlink for no iframe

--- client/src/Components/FeedbackForm.js
+++ client/src/Components/FeedbackForm.js
@@ -9,12 +9,16 @@ export default function FeedbackForm() {
     }, []);
 
     return (
-        <div className={"text-center"}>
+        <div className={"container mt-1"}>
             {loading && (
-                <div className="spinner-border text-light" style={{width: '6rem', height: '6rem'}} role="status">
-                    <span className="visually-hidden">Loading...</span>
+                <div className="text-center">
+                    <div className="spinner-border text-light" style={{width: '6rem', height: '6rem'}} role="status">
+                        <span className="visually-hidden">Loading...</span>
+                    </div>
                 </div>
             )}
+
+            <div className="text-white pt-4 mt-3 text-center">Fill out <a href={"https://forms.gle/LkNevV9iKZNUp5KE7"} target={"_blank"}>this form</a> to tell us more about your issue.</div>
             <iframe
                 id="feedback-form-iframe"
                 src="https://docs.google.com/forms/d/e/1FAIpQLSdrU00XxtYxR9FHDB-yhDaW7L5h1gDo2zDbYR0tt5OP8QlEWA/viewform?embedded=true"
